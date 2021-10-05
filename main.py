22bdbd039c564230bd28b1085b4a1b9eb7de8c6f
FIN443 <jjhun1228@gmail.com>
2021-10-06 00:08:30 +0900

    #2.7 create dashboard

--- main.py
+++ main.py
@@ -1,60 +1,81 @@
-import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+from data import countries_df, totals_df
+from builders import make_table
 
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
+]
 
-conditions = ["confirmed", "deaths", "recovered"]
+app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
-daily_df = pd.read_csv("data/daily_report.csv")
-
-totals_df = (
-    daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    size_max=40,
+    hover_name="Country_Region",
+    locations="Country_Region",
+    locationmode="country names",
+    color="Confirmed",
+    title="Confirmed By Country",
+    template="plotly_dark",
+    color_continuous_scale=px.colors.sequential.Oryel,
+    projection="natural earth",
+    hover_data={
+        "Confirmed": ":,.0f",
+        "Deaths": ":,.0f",
+        "Recovered": ":,.0f",
+        "Country_Region": False,
+    },
 )
-totals_df = totals_df.rename(columns={"index": "condition"})
+bubble_map.update_layout(margin=dict(l=0, r=0, t=40, b=0))
 
-countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
-countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+bars_gragh = px.bar(
+    totals_df,
+    x="condition",
+    y="count",
+    template="plotly_dark",
+    title="Total Global Cases",
+    hover_data={
+        "count": ":,",
+        "condition": False,
+    },
+)
+bars_gragh.update_layout(xaxis=dict(title="Condition"), yaxis=dict(title="Count"))
+
+app.layout = html.Div(
+    style={
+        "minHeight": "100vh",
+        "backgroundColor": "#111111",
+        "color": "white",
+        "fontFamily": "Open Sans, sans-serif",
+    },
+    children=[
+        html.Header(
+            style={
+                "textAlign": "center",
+                "paddingTop": "50px",
+                "marginBottom": 100,
+            },
+            children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
+        ),
+        html.Div(
+            children=[
+                html.Div(children=[dcc.Graph(figure=bubble_map)]),
+                html.Div(children=[make_table(countries_df)]),
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_gragh)]),
+            ]
+        ),
+    ],
+)
 
 
-def make_country_df(country):
-    def make_df(condition):
-        df = pd.read_csv(f"data/time_{condition}.csv")
-        df = df.loc[df["Country/Region"] == country]
-        df = (
-            df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"])
-            .sum()
-            .reset_index(name=condition)
-        )
-        df = df.rename(columns={"index": "date"})
-        return df
-
-    final_df = None
-
-    for condition in conditions:
-        condition_df = make_df(condition)
-        if final_df is None:
-            final_df = condition_df
-        else:
-            final_df = final_df.merge(condition_df)
-
-    return final_df
-
-
-def make_global_df():
-    def make_df(condition):
-        df = pd.read_csv(f"data/time_{condition}.csv")
-        df = (
-            df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
-            .sum()
-            .reset_index(name=condition)
-        )
-        df = df.rename(columns={"index": "date"})
-        return df
-
-    final_df = None
-
-    for condition in conditions:
-        condition_df = make_df(condition)
-        if final_df is None:
-            final_df = condition_df
-        else:
-            final_df = final_df.merge(condition_df)
-    return final_df
+if __name__ == "__main__":
+    app.run_server(debug=True)
